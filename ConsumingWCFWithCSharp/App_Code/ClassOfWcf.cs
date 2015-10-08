920329386ae90bfeed9b89cc240149971f61db70
pankwood <danilo.debiazi@yahoo.com.br>
2015-10-08 16:56:28 -0300

    Consume WCF with Native C#

---
+++ ConsumingWCFWithCSharp/App_Code/ClassOfWcf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class ClassOfWcf
+{
+    public int Field1 { get; set; }
+    public int FieldN { get; set; }
+}
+
+public class CidadeResult
+{
+    public string Cod_Cidade { get; set; }
+    public string NomeCidade { get; set; }
+}
